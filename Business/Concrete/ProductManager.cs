aceb8846698b52f749089c8a7aa43d03e3561ed6
Subhan Masimov <subhanmasimov02@gmail.com>
2021-02-22 11:33:29 +0400

    Camp 11th Day

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -34,10 +34,10 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour == 22)
-            {
-                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
-            }
+            //if (DateTime.Now.Hour == 22)
+            //{
+            //    return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
+            //}
 
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductsListed);
         }
@@ -63,7 +63,7 @@ namespace Business.Concrete
             {
                 return new ErrorDataResult<List<ProductDetailDto>>(Messages.MaintenanceTime);
             }
-                return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetailDtos());
+            return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetailDtos());
         }
     }
 }
