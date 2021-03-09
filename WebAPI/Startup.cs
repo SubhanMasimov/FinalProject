58277d64f52f40a1f3b767405511cf678aefe963
Subhan Masimov <subhanmasimov02@gmail.com>
2021-03-09 14:21:21 +0400

    Camp 15th Day

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,5 +1,7 @@
 using Business.Abstract;
 using Business.Concrete;
+using Core.DependencyResolvers;
+using Core.Extensions;
 using Core.Utilities.IoC;
 using Core.Utilities.Security.Encryption;
 using Core.Utilities.Security.JWT;
@@ -37,9 +39,6 @@ namespace WebAPI
         {
             services.AddControllers();
             //services.AddSingleton<IProductService, ProductManager>();
-            //services.AddSingleton<IProductDal, EfProductDal>();
-
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
@@ -58,7 +57,10 @@ namespace WebAPI
                     };
                 });
 
-            ServiceTool.Create(services);
+            services.AddDependencyResolvers(new ICoreModule[]
+                {
+                    new CoreModule()
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
